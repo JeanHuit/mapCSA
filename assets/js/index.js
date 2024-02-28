4e002f82ead184d07f8295529f706120f8135336
Jean Huit <kollecter@gmail.com>
2024-02-28 15:58:37 +0000

    reworks the cidex code to include indexing and caching

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,49 @@ var map = L.map('map').setView([
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
 
+
+
 var sidebar = document.getElementById('sidebar');
 var searchInput = document.getElementById('searchInput');
 var jsonData = []; // Array to store JSON data
+var index;
+
+var cachedIndex = localStorage.getItem('lunrIndex');
+
 
 fetch('data/ngo_data/csvjson.json').then(response => response.json()).then(data => {
     jsonData = data; // Store JSON data
+    
+// Adds caching ability
+    if (!cachedIndex) 
+    {
+        index = lunr(function () {
+            this.ref('UID');
+            this.field('Name of Project');
+            this.field('Description');
+            this.field('Sponsor-Full');
+            this.field('Sponsor-Abb');
+            this.field('System');
+            this.field('CSA Technology');
+            this.field('subcategory');
+    
+    
+            // Add documents to the index
+            jsonData.forEach(function (doc) {
+                this.add(doc);
+            }, this);
+        });
+    
+        // Cache the index in local storage
+        localStorage.setItem('lunrIndex', JSON.stringify(index));
+    } 
+    else 
+    {
+        // If the index exists in the cache, parse it
+        index = lunr.Index.load(JSON.parse(cachedIndex));
+    }
+
+ 
 });
 
 fetch('data/map_data/ghana_regions.geojson').then(response => response.json()).then(data => {
@@ -39,15 +76,17 @@ fetch('data/map_data/ghana_regions.geojson').then(response => response.json()).t
 function filterResults() {
     var searchString = searchInput.value.toLowerCase();
     sidebar.innerHTML = ''; // Clear sidebar
-    var matchingProjects = jsonData.filter(project => {
-        return project["Name of Project"].toLowerCase().includes(searchString) || project["Description"].toLowerCase().includes(searchString) || project["Region"].toLowerCase().includes(searchString) || project["District"].toLowerCase().includes(searchString);
+
+    var searchResults = index.search(searchString);
+
+    searchResults.forEach(function (result) {
+        var matchingProject = jsonData.find(project => project["UID"] == result.ref); // Find matching project
+        if (matchingProject) {
+            sidebar.innerHTML += `<div class="ui segment"><h3 class="ui header">${matchingProject["Name of Project"]}</h3><p>Description: ${matchingProject["Description"]}</p><p>Region: ${matchingProject["Region"]}</p><a href="details.html?projectId=${matchingProject["UID"]}" class="read-more-link">Read more</a></div>`;
+        }
     });
 
-    // Display matching projects in the sidebar
-    matchingProjects.forEach(project => {
-        sidebar.innerHTML += `<div class="ui segment"><h3 class="ui header">${project["Name of Project"]}</h3><p>Description: ${project["Description"]}</p><p>Region: ${project["Region"]}</p><a href="details.html?projectId=${project["UID"]}" class="read-more-link">Read more</a>
-        </div>`;
-    });
+   
 }
 
 function searchOnEnter(event) {
@@ -62,10 +101,18 @@ function displayDistrictInfo(districtName) { // Check if the district exists in 
     sidebar.innerHTML = '';
     // Clear sidebar
     // Display matching projects in the sidebar
-    districtData.forEach(project => {
-        sidebar.innerHTML += `<div class="ui segment"><h3 class="ui header">${project["Name of Project"]}</h3><p>Description: ${project["Description"]}</p><p>Region: ${project["Region"]}</p><a href="details.html?projectId=${project["UID"]}" class="read-more-link">Read more</a>
-        </div>`;
-    });
+    if (districtData.length > 0){
+        districtData.forEach(project => {
+            sidebar.innerHTML += `<div class="ui segment"><h3 class="ui header">${project["Name of Project"]}</h3><p>Description: ${project["Description"]}</p><p>Region: ${project["Region"]}</p><a href="details.html?projectId=${project["UID"]}" class="read-more-link">Read more</a>
+            </div>`;
+        });
+
+    }
+    else
+    {
+        sidebar.innerHTML += `<div class="ui segment"><p>No data in selected region</p></div>`;
+    }
+    
 }
 
 
